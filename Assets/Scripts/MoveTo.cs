164e79317ea85a339f53795bdb2bc003ede7a3f0
Dnk095 <darcnikill@gmail.com>
2024-10-17 14:58:57 +1200

    Update MoveTo.cs

--- Assets/Scripts/MoveTo.cs
+++ Assets/Scripts/MoveTo.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 public class MoveTo : MonoBehaviour
 {
     [SerializeField] private float _duration;
+    [SerializeField] private int _repeats;
     [SerializeField] private Vector3 _position;
     [SerializeField] private LoopType _loopType;
-    [SerializeField] private int _repeats;
 
-    private void Update()
+    private void Start()
     {
         transform.DOMove(_position, _duration).SetLoops(_repeats, _loopType);
     }
